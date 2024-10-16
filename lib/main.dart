645c4cb4eb5fd0dcb12073f5c6a837037e42ab9c
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-10-16 11:01:24 +0200

    implemented "OrderNowClipRRect"

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:developer';
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:project_14_jp_app_design_challenge/second_page.dart';
 
@@ -44,79 +43,63 @@ class MainApp extends StatelessWidget {
                   ),
                 ),
               ),
-              Positioned(
+              const Positioned(
                 top: 650,
                 right: 20,
                 left: 20,
                 bottom: 50,
+                child: OrderNowClipRRect(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(32)),
+class OrderNowClipRRect extends StatelessWidget {
+  const OrderNowClipRRect({super.key});
 
-                  // child: const Color.fromARGB(195, 150, 21, 21),
-                  // borderOnForeground: true,
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
-                    // blendMode: BlendMode.difference, // dunkel mit overlay
-                    // blendMode: BlendMode.hardLight, // sieht besser aus
-                    // blendMode: BlendMode.screen, // auch zu gebrauchen
-                    blendMode: BlendMode.src, // passt fast zur Vorlage
-                    child: const Column(
-                      children: [
-                        SizedBox(height: 30),
-                        Text(
-                          "Feeling Snackish Today?",
-                          style: TextStyle(
-                              fontSize: 26,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(32, 8, 32, 0),
-                          child: Text(
-                            "Explore Angi's most popular snack selection and get instantly happy",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: 24),
-                        ButtonOrderNow(),
-                      ],
-                    ),
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(32)),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+        // blendMode: BlendMode.difference, // dunkel mit overlay
+        // blendMode: BlendMode.hardLight, // sieht besser aus
+        // blendMode: BlendMode.screen, // auch zu gebrauchen
+        // blendMode: BlendMode.src, // passt fast zur Vorlage
+        child: Container(
+          decoration: BoxDecoration(
+            //color: Colors.white.withOpacity(0.03),
+            borderRadius: BorderRadius.circular(32),
+            border: Border.all(color: Colors.white, width: 0.2),
+          ),
+          child: const Column(
+            children: [
+              SizedBox(height: 30),
+              Text(
+                "Feeling Snackish Today?",
+                style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(32, 8, 32, 0),
+                child: Text(
+                  "Explore Angi's most popular snack selection and get instantly happy",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
                   ),
                 ),
-
-                // child: Card(
-                //   color: Color.fromARGB(195, 150, 21, 21),
-                //   borderOnForeground: true,
-                //   child: Column(
-                //     children: [
-                //       SizedBox(height: 30),
-                //       Text(
-                //         "Feeling Snackish Today?",
-                //         style: TextStyle(
-                //             fontSize: 26,
-                //             fontWeight: FontWeight.bold,
-                //             color: Colors.white),
-                //       ),
-                //       Padding(
-                //         padding: EdgeInsets.fromLTRB(32, 8, 32, 0),
-                //         child: Text(
-                //           "Explore Angi's most popular snack selection and get instantly happy",
-                //           textAlign: TextAlign.center,
-                //           style: TextStyle(
-                //             fontSize: 14,
-                //             color: Colors.white,
-                //           ),
-                //         ),
-                //       ),
-                //     ],
-                //   ),
-                // ),
               ),
+              SizedBox(height: 24),
+              ButtonOrderNow(),
             ],
           ),
         ),
